242eb27013982bbe48b565f0e29faadc8188e55d
rajaryan2007 <rajaryan150149@gmail.com>
2025-08-29 02:58:57 +0530

    backendDone

--- backend/routes/image-routes.js
+++ backend/routes/image-routes.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const router = express.Router();
-const {deleteImagefromDB,fetchImageController,fetchSingleImage,ImageUploadSystem} = require("../controller/image-controller")
-const isadminUser = require("../middleware/admin-middleware") ;
+const { deleteImagefromDB, fetchImageController, fetchSingleImage, ImageUploadSystem } = require("../controller/image-controller")
+
+const isadminUser = require("../middleware/admin-middleware");
 const uploadmiddleware = require("../middleware/upload-image-middleware")
 const authMiddleware = require("../middleware/user-middleware")
-router.post("/upload-image",isadminUser,ImageUploadSystem);
-router.get("/Single-image",fetchSingleImage);
-router.delete("/delete-image",isadminUser,deleteImagefromDB);
-router.get("all-image",fetchImageController);
+
+
+
+router.post("/upload-image", isadminUser, ImageUploadSystem);
+router.get("/Single-image", fetchSingleImage);
+router.delete("/delete-image", isadminUser, deleteImagefromDB);
+router.get("all-image", fetchImageController);
+
+module.exports = router
